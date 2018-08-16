63be0d677533eaa0dd7501e328b90ba424a1a292
Christgo <christopher.ordorica@my.jcu.edu.au>
2018-08-16 11:44:25 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,8 @@
 """Christopher Ordorica"""
+
+MIN_LENGTH = 6
+
+password_length = len(input("Please enter a password that is {} characters long.".format(MIN_LENGTH)))
+while password_length < MIN_LENGTH:
+    password_length = len(input("Invalid Password. \nPlease enter a valid password"))
+print("*" * password_length)
